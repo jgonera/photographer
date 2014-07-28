6ad0f49315422f6b249924001f30e13941b1cfeb
Juliusz Gonera <jgonera@gmail.com>
2014-07-27 23:36:15 -0700

    Optimize MovingFrameComparator
    Store precomputed number of different pixels for each column, update
    counts when increasing row (moving frame down).

--- lib/photographer/comparators/moving_frame_comparator.rb
+++ lib/photographer/comparators/moving_frame_comparator.rb
@@ -3,6 +3,56 @@ require "oily_png"
 module Photographer
   module Comparators
     class MovingFrameComparator
+      class Differentiator
+        attr_reader :frame_difference
+
+        def initialize(image_a, image_b, frame_size: 100)
+          @image_a = image_a
+          @image_b = image_b
+          @frame_size = frame_size
+
+          @width = image_a.width
+          @column_sums = []
+
+          @width.times do |x|
+            sum = 0
+
+            @frame_size.times do |y|
+              sum += 1 unless is_equal(x, y)
+            end
+
+            @column_sums << sum
+          end
+
+          @x = 0
+          @y = 0
+          @frame_difference = @column_sums[0, @frame_size].inject(:+)
+        end
+
+        def move_right
+            @frame_difference -= @column_sums[@x]
+            @frame_difference += @column_sums[@x + @frame_size]
+            @x += 1
+        end
+
+        def move_down
+          end_y = @y + @frame_size
+
+          @width.times do |x|
+            @column_sums[x] -= 1 unless is_equal(x, @y)
+            @column_sums[x] += 1 unless is_equal(x, end_y)
+          end
+
+          @frame_difference = @column_sums[0, @frame_size].inject(:+)
+          @y += 1
+          @x = 0
+        end
+
+        def is_equal(x, y)
+          @image_a[x, y] == @image_b[x, y]
+        end
+      end
+
       def initialize(max_difference: 0.1, frame_size: 100)
         @max_difference = max_difference
         @frame_size = frame_size
@@ -12,31 +62,19 @@ module Photographer
       def compare(path_a, path_b)
         @image_a = ChunkyPNG::Image.from_file path_a
         @image_b = ChunkyPNG::Image.from_file path_b
+        differentiator = Differentiator.new(@image_a, @image_b, frame_size: @frame_size)
 
         x_limit = @image_a.width - @frame_size
         y_limit = @image_a.height - @frame_size
 
-        y_limit.times do |y_offset|
-          x_limit.times do |x_offset|
-            compare_frame(y_offset, x_offset)
+        y_limit.times do
+          x_limit.times do
+            raise Photographer::ComparisonError if differentiator.frame_difference.to_f / @frame_area > @max_difference
+            differentiator.move_right
           end
+
+          differentiator.move_down
         end
-      end
-
-      protected
-
-      def compare_frame(y_offset, x_offset)
-        diff_count = 0
-
-        @frame_size.times do |frame_y|
-          y = y_offset + frame_y
-          @frame_size.times do |frame_x|
-            x = x_offset + frame_x
-            diff_count += 1 unless @image_a[x, y] == @image_b[x, y]
-          end
-        end
-
-        raise Photographer::ComparisonError if diff_count.to_f / @frame_area > @max_difference
       end
     end
   end
--- spec/comparators/moving_frame_comparator_spec.rb
+++ spec/comparators/moving_frame_comparator_spec.rb
@@ -1,5 +1,19 @@
 require "spec_helper"
 
+class FakeImage
+  def initialize(matrix)
+    @matrix = matrix
+  end
+
+  def [](x, y)
+    @matrix[y][x]
+  end
+
+  def width
+    @matrix[0].length
+  end
+end
+
 describe Photographer::Comparators::MovingFrameComparator do
   subject { Photographer::Comparators::MovingFrameComparator.new(frame_size: 5) }
 
@@ -24,6 +38,105 @@ describe Photographer::Comparators::MovingFrameComparator do
           subject.compare test_snap_path("white_10x10"), test_snap_path("moving_frame_scattered")
         }.to raise_error(Photographer::ComparisonError)
       end
+    end
+  end
+end
+
+describe Photographer::Comparators::MovingFrameComparator::Differentiator do
+  let(:blank_image) {
+    FakeImage.new([
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ])
+  }
+
+  it "calculates the difference for frame at x=0" do
+    subject = Photographer::Comparators::MovingFrameComparator::Differentiator.new(
+      blank_image,
+      FakeImage.new([
+        [1, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+      ]),
+      frame_size: 2
+    )
+    expect(subject.frame_difference).to eq 1
+  end
+
+  describe "#move_right" do
+    it "calculates the difference for frame at x=1" do
+      subject = Photographer::Comparators::MovingFrameComparator::Differentiator.new(
+        blank_image,
+        FakeImage.new([
+          [0, 0, 1],
+          [0, 0, 0],
+          [0, 0, 0]
+        ]),
+        frame_size: 2
+      )
+      subject.move_right
+      expect(subject.frame_difference).to eq 1
+    end
+
+    it "subtracts previous column's sum" do
+      subject = Photographer::Comparators::MovingFrameComparator::Differentiator.new(
+        blank_image,
+        FakeImage.new([
+          [1, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]
+        ]),
+        frame_size: 2
+      )
+      subject.move_right
+      expect(subject.frame_difference).to eq 0
+    end
+  end
+
+  describe "#move_down" do
+    it "calculates the difference for frame at y=1" do
+      subject = Photographer::Comparators::MovingFrameComparator::Differentiator.new(
+        blank_image,
+        FakeImage.new([
+          [0, 0, 0],
+          [0, 0, 0],
+          [1, 0, 0]
+        ]),
+        frame_size: 2
+      )
+      subject.move_down
+      expect(subject.frame_difference).to eq 1
+    end
+
+    it "subtracts previous row's values" do
+      subject = Photographer::Comparators::MovingFrameComparator::Differentiator.new(
+        blank_image,
+        FakeImage.new([
+          [1, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]
+        ]),
+        frame_size: 2
+      )
+      subject.move_down
+      expect(subject.frame_difference).to eq 0
+    end
+
+    it "resets x to zero" do
+      subject = Photographer::Comparators::MovingFrameComparator::Differentiator.new(
+        blank_image,
+        FakeImage.new([
+          [0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 1]
+        ]),
+        frame_size: 2
+      )
+      subject.move_right
+      subject.move_down
+      subject.move_right
+      expect(subject.frame_difference).to eq 1
     end
   end
 end
